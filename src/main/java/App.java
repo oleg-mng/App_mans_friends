21491aac1d854ef9049363d9580e6e6826833af3
oleg <legik7127@gmail.com>
2023-04-27 17:00:50 +0600

    added class Comand

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.net.Socket;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
@@ -10,8 +11,12 @@ public class App {
         ArrayList<MansFriend> arrayAnimal = new ArrayList<>();
 
         AddAnimal.addSomePetAnimal(arrayAnimal, 2, "Aura", LocalDate.of(2018, 01, 22), "pet", 1);
-        AddAnimal.addSomePetAnimal(arrayAnimal, 3, "Inga", LocalDate.of(2021, 03, 18), "pack", 6);
-        AddAnimal.addSomePetAnimal(arrayAnimal, 4, "Benta", LocalDate.of(2017, 07, 29), "pack",7);
+        AddAnimal.addSomePetAnimal(arrayAnimal, 3, "Inga", LocalDate.of(2021, 03, 18), "pet", 2);
+        AddAnimal.addSomePetAnimal(arrayAnimal, 4, "Benta", LocalDate.of(2022, 05, 03), "pet",3);
+        AddAnimal.addSomePetAnimal(arrayAnimal, 5, "Roxy", LocalDate.of(2019, 02, 07), "pack",4);
+        AddAnimal.addSomePetAnimal(arrayAnimal, 6, "Ava", LocalDate.of(2013, 05, 27), "pack",5);
+        AddAnimal.addSomePetAnimal(arrayAnimal, 7, "Penny", LocalDate.of(2015, 10, 21), "pack",6);
+        System.out.println("Введены данные по следующим животным:");
         AddAnimal.printList(arrayAnimal);
 
     }
